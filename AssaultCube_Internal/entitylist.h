eda13b631983fb5f316a6db181b050a5e615f822
cpo <DevPC>
2025-03-31 11:57:38 +0100

    entity list is now working, will add obtaining the names of players in game next

--- AssaultCube_Internal/entitylist.h
+++ AssaultCube_Internal/entitylist.h
@@ -8,26 +8,22 @@ void getEntList()
 {
 	std::cout << "\n\ngetEntList()";
 	engine client{};
+	entity op{};
 	uintptr_t base = client.BaseMod;
 	
-	uintptr_t* e_list = (uintptr_t*)(base + offsets::entitylist);
-	std::cout << "\nE_list: " << e_list << " " << &e_list;
-	const int i_playerNum = *(int*)(base + offsets::numOfPlayers);
-	std::cout << "\nPlayerNum: " << i_playerNum << " " << &i_playerNum;
+
+	for (unsigned int i = 0x04; i < 128; i += 4)
+	{
+		uintptr_t entPosAddr = mem::FindDMAAddy(client.BaseMod + 0x18AC04, { i, offsets::position });
+		op.position = (Vec3*)(entPosAddr);
+		std::cout << "\nPosition [x | y | z] " << "[ " << op.position->x << " | " << op.position->y << " | " << op.position->z;
+	}
 	
 
-	/*for (int i = 4; i < 128; i += 4)
-	{
-		std::cout << "\nEntList loop started\n";
-		int count{1};
-		int ent = *(int*)(e_list + i);
-		int ent_hp = *(int*)(ent + offsets::health);
-		std::cout << "\n Entity " << count << " HP: " << ent_hp << "\n";
-		count++;
-		Sleep(3000);
-	}*/
+	
 
-	std::cout << "\n\nEntListLoop_Complete";
+
+	std::cout << "\n\nEntListLoop_Complete\n\n";
 	system("pause");
 }
 
